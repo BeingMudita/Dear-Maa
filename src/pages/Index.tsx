4d567ed17652198981fe0ebcf4aa9b7ab3d85a8c
Mudita Jain <mudita21jain@gmail.com>
2025-05-06 17:47:15 +0530

    `Removed unused components and files, updated index page`

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,11 @@
 
-import Hero from "@/components/Hero";
-import Timeline from "@/components/Timeline";
 import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
     <div className="min-h-screen flex flex-col bg-background">
       <main className="flex-grow">
-        <Hero />
-        <Timeline />
+        
       </main>
       <Footer />
     </div>
